f91c9ca049e9f58ea8d05d2c427e22d3cdcd4b2c
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2025-05-31 21:38:59 +0530

    feat: Integrate browser-polyfill for cross-browser compatibility, update manifest for Gecko settings, and include polyfill in options and popup pages.

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,13 @@
+// The try/catch is to avoid errors in Chrome.
+try {
+  importScripts("browser-polyfill.js");
+} catch (e) {
+  console.error(
+    "Failed to import browser-polyfill.js. If you are in Chrome, this is expected and can be ignored.",
+    e
+  );
+}
+
 importScripts("auth.js");
 
 // This is the background service worker for the extension.
